101d204f6e73eda2186c9bd9da2a45b702c2f2da
azkaZubish <azkazubish@gmail.com>
2025-09-20 22:17:14 +0530

    modified the path for the pictures

--- src/FeatureSection.jsx
+++ src/FeatureSection.jsx
@@ -1,3 +1,7 @@
+import feature1 from "./assets/feature1.png"
+import feature2 from "./assets/feature2.png"
+import feature3 from "./assets/feature3.png"
+
 export default function FeatureSection() {
   return (
     <section className="bg-white py-12">
@@ -11,19 +15,19 @@ export default function FeatureSection() {
           
           {/* Feature 1 */}
           <div className="p-8 bg-[#E6F0FA] rounded-2xl shadow hover:shadow-lg transition flex flex-col items-center text-center">
-            <img src="./src/assets/feature1.png" alt="Protection" className="w-32 h-32 mb-6" />
+            <img src={feature1} alt="Protection" className="w-32 h-32 mb-6" />
             <h4 className="text-xl font-semibold text-blue-900">Protection from Misinformation</h4>
           </div>
 
           {/* Feature 2 */}
           <div className="p-8 bg-[#E6F0FA] rounded-2xl shadow hover:shadow-lg transition flex flex-col items-center text-center">
-            <img src="./src/assets/feature2.png" alt="Analysis" className="w-32 h-32 mb-6" />
+            <img src={feature2} alt="Analysis" className="w-32 h-32 mb-6" />
             <h4 className="text-xl font-semibold text-blue-900">Balanced Analysis</h4>
           </div>
 
           {/* Feature 3 */}
           <div className="p-8 bg-[#E6F0FA] rounded-2xl shadow hover:shadow-lg transition flex flex-col items-center text-center">
-            <img src="./src/assets/feature3.png" alt="Verified" className="w-32 h-32 mb-6" />
+            <img src={feature3} alt="Verified" className="w-32 h-32 mb-6" />
             <h4 className="text-xl font-semibold text-blue-900">Verified Facts</h4>
           </div>
 
